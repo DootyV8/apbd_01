b307ff3be126dccd98f1d7860bb0139b064e9d96
DootyV8 <s30515@pjwstk.edu.pl>
2025-03-03 11:51:06 +0100

    task 5 argument 2

--- apbd_01_task2/apbd_01_task2/MaxValue.cs
+++ apbd_01_task2/apbd_01_task2/MaxValue.cs
@@ -1,16 +1,16 @@
-﻿
-double MaxValue(int[] numbers)
-{
-    double maxValue = 0;
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        if (numbers[i] > maxValue)
-        {
-            maxValue = numbers[i];
-        }
-    }
-    return maxValue;
-}
-
-
-int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
+﻿//
+// double MaxValue(int[] numbers)
+// {
+//     double maxValue = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > maxValue)
+//         {
+//             maxValue = numbers[i];
+//         }
+//     }
+//     return maxValue;
+// }
+//
+//
+// int[] arr = {1,2,3,4,5,6,7,8,9,10,11};
--- apbd_01_task2/apbd_01_task2/Program.cs
+++ apbd_01_task2/apbd_01_task2/Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// double GetAverage(int[] numbers)
-// {
-//     double average = 0;
-//     for (int i = 0; i < numbers.Length; i++)
-//     {
-//          average += numbers[i];
-//     }
-//     
-//     return average / numbers.Length;
-// }
-//
-//
-// int[] arr = { 1, 2, 3, 4 };
-//
-// Console.WriteLine(GetAverage(arr));
-// Console.WriteLine(GetAverage(arr));
+double GetAverage(int[] numbers)
+{
+    double average2 = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+         average2 += numbers[i];
+    }
+    
+    return average2 / numbers.Length;
+}
+
+
+int[] arr = { 1, 2, 3, 4 };
+
+Console.WriteLine(GetAverage(arr));
+Console.WriteLine(GetAverage(arr));
