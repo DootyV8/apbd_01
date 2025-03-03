0ca05029b60f692c121fb9b950ecc785c3332fab
DootyV8 <s30515@pjwstk.edu.pl>
2025-03-03 09:47:53 +0100

    costam

--- apbd_01_task2/apbd_01_task2/Program.cs
+++ apbd_01_task2/apbd_01_task2/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello there");
- 
+double GetAverage(int[] numbers)
+{
+    return 0;
+}
+
+
+int[] arr = { 1, 2, 3, 4 };
+
+GetAverage(arr);
