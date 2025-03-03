f77c1032c0e41cfa2ac0ebf55f694d2e5b3c13d1
DootyV8 <s30515@pjwstk.edu.pl>
2025-03-03 10:43:16 +0100

    actually working average calculation

--- apbd_01_task2/apbd_01_task2/Program.cs
+++ apbd_01_task2/apbd_01_task2/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello there");
+
 double GetAverage(int[] numbers)
 {
-    return 0;
+    double average = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+         average += numbers[i];
+    }
+    
+    return average / numbers.Length;
 }
 
 
 int[] arr = { 1, 2, 3, 4 };
 
-GetAverage(arr);
+Console.WriteLine(GetAverage(arr));
